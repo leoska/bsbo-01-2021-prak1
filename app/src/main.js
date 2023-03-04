427b6d86229c8e1c3680b2d7705393d67901df3d
MeNoobly <84956743974@mail.ru>
2023-03-04 17:30:18 +0300

    rixy commit

---
+++ app/src/main.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+const button = document.querySelector(".form__button");
+const inputs = document.querySelectorAll(".form__input");
+const text = document.querySelector(".text");
+
+const sayHello = (event) => {
+    event.preventDefault();
+    text.textContent = `Здравствуйте, ${inputs[0].value} ${inputs[1].value}`;
+    inputs[0].value = "";
+    inputs[1].value = "";
+}
+
+button.addEventListener("click", sayHello);
